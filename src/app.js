2d55a53e15c0b5d94f8e1dd106d813b181098946
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-05-27 09:11:11 +0000

    add helmet middleware for enhanced security

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { fileSize } from "./constants.js";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
-
 const app = express();
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true, limit: fileSize }));
 app.use(express.json({ limit: fileSize }));
 app.use(express.static("public"));
