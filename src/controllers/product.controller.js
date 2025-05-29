13672672fb48f6f641ce21c71181e785ae1d2f70
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-05-29 12:10:52 +0000

    add product routes: implement getAllProducts, getProductById, updateProductById, deleteProductById, getMyProducts, and searchProducts

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -84,3 +84,136 @@ export const createProduct = asyncHandler(async (req, res) => {
         .status(201)
         .json(new ApiResponse(201, product, "product created successfully"));
 });
+
+export const getAllProducts = asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find({})
+        .skip(skip)
+        .limit(limit)
+        .populate("createdBy", "username email")
+        .select("-__v")
+        .sort("-createdAt");
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { page, limit, products },
+                "products fetched successfully with pagination"
+            )
+        );
+});
+
+export const getProductById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findById(id)
+        .populate("createdBy", "username email")
+        .select("-__v");
+
+    if (!product) throw new ApiError(404, "Product not found");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, product, "Product fetched successfully"));
+});
+
+export const updateProductById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate({ _id: id }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+        .populate("createdBy", "username email")
+        .select("-__v");
+    if (!product) throw new ApiError(404, "product not found");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, product, "product updated successfully"));
+});
+
+export const deleteProductById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete({ _id: id })
+        .populate("createdBy", "username email")
+        .select("-__v");
+    if (!product) throw new ApiError(404, "product not found");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, product, "product deleted successfully"));
+});
+
+export const getMyProducts = asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find({ createdBy: req.user._id })
+        .skip(skip)
+        .limit(limit)
+        .populate("createdBy", "username email")
+        .select("-__v")
+        .sort("-createdAt");
+    if (products.length === 0)
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    { page, limit, products },
+                    "you have not created any products yet"
+                )
+            );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { page, limit, products },
+                "your products fetched successfully"
+            )
+        );
+});
+
+export const searchProducts = asyncHandler(async (req, res) => {
+    const { query } = req.query;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    if (!query)
+        return res
+            .status(400)
+            .json(new ApiResponse(400, null, "query parameter is required"));
+
+    const products = await Product.find({
+        $or: [
+            { name: { $regex: query, $options: "i" } },
+            { description: { $regex: query, $options: "i" } },
+        ],
+    })
+        .skip(skip)
+        .limit(limit)
+        .populate("createdBy", "username email")
+        .select("-__v")
+        .sort("-createdAt");
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { page, limit, products },
+                "products fetched successfully"
+            )
+        );
+});
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.controller.js";
+import {
+    createProduct,
+    deleteProductById,
+    getAllProducts,
+    getMyProducts,
+    getProductById,
+    searchProducts,
+    updateProductById,
+} from "../controllers/product.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
@@ -8,5 +16,17 @@ const router = Router();
 router
     .route("/create-product")
     .post(verifyJWT, upload.array("images"), createProduct);
+
+router.route("/get-all-products").get(verifyJWT, getAllProducts);
+
+router.route("/get-user-product/:id").get(verifyJWT, getProductById);
+
+router.route("/update-user-product/:id").patch(verifyJWT, updateProductById);
+
+router.route("/delete-user-product/:id").delete(verifyJWT, deleteProductById);
+
+router.route("/get-my-products").get(verifyJWT, getMyProducts);
+
+router.route("/search-products").get(verifyJWT, searchProducts);
 
 export default router;
